cd27f5cf0e25ab6523ad576b2a8f6a44583cf349
Mario23junior <mariojunior3251@gmail.com>
2021-08-03 02:54:23 -0300

    Criando controller e criando endpoint de listar todos os dados cadastrados

---
+++ src/main/java/com/project/agenda/Controller/PessoaController.java
@@ -0,0 +1,28 @@
+package com.project.agenda.Controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.project.agenda.Model.Pessoa;
+import com.project.agenda.Service.PessoaService;
+
+@RestController
+@RequestMapping("/v1/api/agenda/pessoa")
+public class PessoaController {
+   
+	private PessoaService pessoaService;
+	
+	public PessoaController(PessoaService pessoaService) {
+		this.pessoaService = pessoaService;
+ 	}
+	
+	@GetMapping
+	public List<Pessoa> listingAll(){
+		return pessoaService.listAllData();
+	}
+	
+	
+}
