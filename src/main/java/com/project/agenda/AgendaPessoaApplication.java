a87d163cceb0f3a133443eff03a85bbbe6ae3db5
Mario23junior <mariojunior3251@gmail.com>
2021-08-03 02:02:43 -0300

    Iniciando projeto de agenda(CRUD)

---
+++ src/main/java/com/project/agenda/AgendaPessoaApplication.java
@@ -0,0 +1,13 @@
+package com.project.agenda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaPessoaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaPessoaApplication.class, args);
+	}
+
+}
